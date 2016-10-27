381abc201f08f38a3fe17d81bcebfbbe8b7895c2
Henry Reed <henryreed@Henrys-MBP.home>
2016-10-27 13:44:40 -0600

    finished project

--- service.js
+++ service.js
@@ -6,13 +6,13 @@ angular.module('userProfiles').service('mainService', function($http, $q) {
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
     }).then(function(response) {
-      var parsedResponse = response.data.data
+      var parsedResponse = response.data.data;
       for (var i = 0; i < parsedResponse.length; i++){
-        parsedResponse[i].first_name = 'Ralf'
+        parsedResponse[i].first_name = 'Ralf';
       }
-      deferred.resolve(parsedResponse)
-    })
+      deferred.resolve(parsedResponse);
+    });
     return deferred.promise;
-  }
+  };
 
 });
